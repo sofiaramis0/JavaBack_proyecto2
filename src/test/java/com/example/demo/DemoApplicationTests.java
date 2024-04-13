419ed4adfb1ab77300cde9e37643010023314b7d
hrubiogi <hrubiogi@uoc.edu>
2024-04-13 14:33:04 +0200

    Docker

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ContextConfiguration(classes = TestConfig.class) // Reemplaza TestConfig con tu clase de configuración de prueba
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext context;
 
+    @Test
+    void contextLoads() {
+        // Verificar que el contexto de la aplicación se carga correctamente
+        assertThat(context).isNotNull();
+    }
 }
